4849ff18a84273d3cd8de28ecd3a1e1fbf45934b
sanket-prasad <53733446+sanket-prasad@users.noreply.github.com>
2019-08-21 21:03:35 +0530

    Update problem3.java

--- problem3.java
+++ problem3.java
@@ -2,32 +2,29 @@ public class problem3
 {
     public static void main (String args[])
     {
-        long g=0Lx;
+        long i=0L;
         long n = 600851475143L;
 
-        loop1: for(long f=n; f>1; f=f-2)
+        loop1: for(i=n; i>1; i=i-2)
         {
+            int c=0;
+            int sr=(int)Math.sqrt(i);
+            for(int f=3;f<sr;f++)
+            {
            
-           if(n%f==0)
-           {
-              int c=0;
-              for(long x=1;x<=f;x++)
+              if(sr%f==0)
               {
-                  if(f%x==0)
-                  {
-                      c++;
-                      if(c==3)
-                      continue loop1;
-                  }
+               c++;
+               continue loop1;
               }
-              if(c==2 && f>g)
-              g=f;
-
-           } 
-        
-
+              
+            }
+            if(c==0)
+            {
+                System.out.println(i);
+                break loop1;
+            }
         }
-        System.out.println(g);
         
     }
 }
