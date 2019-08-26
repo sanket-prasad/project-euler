c84f70aa3e4134cb7339aeb967d6686c24944d3c
sanket-prasad <53733446+sanket-prasad@users.noreply.github.com>
2019-08-26 11:35:06 +0530

    Update problem3.java

--- problem3.java
+++ problem3.java
@@ -7,22 +7,21 @@ public class problem3
 
         loop1: for(i=n; i>1; i=i-2)
         {
-            int c=0;
-            int sr=(int)Math.sqrt(i);
-            for(int f=3;f<sr;f++)
+            if(n%i==0)
             {
-           
-              if(sr%f==0)
-              {
-               c++;
-               continue loop1;
-              }
-              
-            }
-            if(c==0)
-            {
-                System.out.println(i);
-                break loop1;
+                int c=0;
+                for(int f=2;f<=Math.sqrt(i);f++)
+                {
+                    if(i%f==0)
+                    {
+                        continue loop1;
+                    }
+                }
+                if(c==0)
+                {
+                    System.out.println(i);
+                    break loop1;
+                }
             }
         }
         
