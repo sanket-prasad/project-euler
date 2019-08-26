9d3b6f554f860529fc05d8267e9b4fcfeb8a80fb
sanket-prasad <53733446+sanket-prasad@users.noreply.github.com>
2019-08-26 11:28:55 +0530

    Update problem6.java

--- problem6.java
+++ problem6.java
@@ -2,14 +2,9 @@ public class problem6
 {
     public static void main(String args[])
     {
-        int sum, sumsq, psum;
-        sum=0;sumsq=0;psum=0;
-        for(int i=1;i<=100;i++)
-        {
-            sum=sum+i;
-            sumsq=sumsq+(i*i);
-        }
-        psum=sum*sum;
-        System.out.println(psum-sumsq);
+        int sum=0; int sumsq=0;
+        sum=(100*101)/2;
+        sumsq=(100*101*201)/6;
+        System.out.println((sum*sum)-sumsq);
     }
 }
